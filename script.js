2a2a6e748b11d6262352015c76d08574f44919ef
Akem-Ben <107713743+Akem-Ben@users.noreply.github.com>
2023-03-19 19:30:50 +0100

    homepage

---
+++ script.js
@@ -0,0 +1,10 @@
+// import productz from "./products/products.js";
+
+// let prod = document.querySelector('.pro')
+
+// prod.innerHTML = productz.map((arg)=>`
+// <div>product name=${arg.name}</div>
+// <div>product height=${arg.height}</div>
+// <div>product date=${arg.date}</div>
+// <div>product image= <img src=${arg.image} alt="image"></div>
+// `)
